d16304ee3f5a644fb1802b96393dbf09077db6a8
Milad Abdi <miladabdi1374@gmail.com>
2021-08-08 06:37:20 +0430

    Add FirstMenu

---
+++ src/main/java/ir/maktab/todo/front/menus/FirstMenu.java
@@ -0,0 +1,40 @@
+package ir.maktab.todo.front.menus;
+
+
+import ir.maktab.todo.domain.User;
+
+import java.sql.SQLException;
+
+public class FirstMenu extends Menu implements RunnableMenu<Void> {
+
+    public FirstMenu() {
+        super(new String[]{"Login", "Sign Up", "Exit"});
+    }
+
+    // This method do all the first menu tasks
+    @Override
+    public Void runMenu() throws SQLException {
+        while (true) {
+            showMenu();
+            switch (getChosenItem()) {
+                case 1:
+//                    User loginUser = ApplicationContext.userService.login();
+//                    if(!Objects.isNull(loginUser)) {
+//                        new UserMenu(loginUser).runMenu();
+//                    }
+                    break;
+                case 2:
+                    User signUpUser = getSignUpUserInfo();
+//                    ApplicationContext.userService.signUp(signUpUser);
+                    break;
+                case 3:
+                    if (new CheckMenu("Are you sure you want to exit?").runMenu()) return null;
+                    else break;
+            }
+        }
+    }
+
+    private User getSignUpUserInfo() {
+        return null;
+    }
+}
