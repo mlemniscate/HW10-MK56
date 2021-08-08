0f1125698e451917b49b366e2242348dd30bbd11
Milad Abdi <miladabdi1374@gmail.com>
2021-08-08 06:30:31 +0430

    Manipulate ApplicationContext

--- src/main/java/ir/maktab/todo/ApplicationContext.java
+++ src/main/java/ir/maktab/todo/ApplicationContext.java
@@ -6,8 +6,8 @@ import ir.maktab.todo.service.impl.ActivityServiceImpl;
 import ir.maktab.todo.service.impl.UserServiceImpl;
 
 public class ApplicationContext {
-    public static final UserServiceImpl cartService = new UserServiceImpl(new UserRepositoryImpl(ToDoApp.getEntityManager()));
+    public static final UserServiceImpl userService = new UserServiceImpl(new UserRepositoryImpl(ToDoApp.getEntityManager()));
 
-    public static final ActivityServiceImpl customerService = new ActivityServiceImpl(new ActivityRepositoryImpl(ToDoApp.getEntityManager()));
+    public static final ActivityServiceImpl activityService = new ActivityServiceImpl(new ActivityRepositoryImpl(ToDoApp.getEntityManager()));
 
 }
