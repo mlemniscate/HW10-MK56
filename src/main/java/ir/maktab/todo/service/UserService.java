5fc88d6cfc1edad1d26428cb2d3b5f68ec8aaa90
Milad Abdi <miladabdi1374@gmail.com>
2021-08-08 06:37:04 +0430

    Manipulate UserService

--- src/main/java/ir/maktab/todo/service/UserService.java
+++ src/main/java/ir/maktab/todo/service/UserService.java
@@ -4,4 +4,7 @@ import ir.maktab.todo.base.service.BaseService;
 import ir.maktab.todo.domain.User;
 
 public interface UserService extends BaseService<User, Long> {
+    User logIn(String username, String password);
+
+    User signUp(User user);
 }
